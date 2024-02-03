1c2eed6f169dbb4844531ad51c742063f4f27ff0
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-02-03 00:19:38 -0300

    feat: ensure confirmed bookings are shown on home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,9 +5,29 @@ import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
 
 export default async function Home() {
-  const barbershops = await db.barbershop.findMany({});
+  const session = await getServerSession(authOptions);
+
+  const [barbershops, confirmedBookings] = await Promise.all([
+    db.barbershop.findMany({}),
+    session?.user
+      ? db.booking.findMany({
+          where: {
+            userId: (session.user as any).id,
+            date: {
+              gte: new Date(),
+            },
+          },
+          include: {
+            service: true,
+            barbershop: true,
+          },
+        })
+      : Promise.resolve([]),
+  ]);
 
   return (
     <div>
@@ -23,12 +43,17 @@ export default async function Home() {
         <Search />
       </div>
 
-      {/* <div className="mt-6 px-5">
-        <h2 className="mb-3 text-xs font-bold uppercase text-gray-400">
+      <div className="mt-6">
+        <h2 className=" mb-3 pl-5 text-xs font-bold uppercase text-gray-400">
           Agendamentos
         </h2>
-        <BookingItem />
-      </div> */}
+
+        <div className="mt-6 flex gap-3 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+          {confirmedBookings.map((booking) => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+        </div>
+      </div>
 
       <div className="mt-6">
         <h2 className="mb-3 px-5 text-xs font-bold uppercase text-gray-400">
